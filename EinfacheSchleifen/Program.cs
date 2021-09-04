a4f57032db1b1593e4b3d351bed1196b49dcade3
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:24:34 +0200

    Projektdateien hinzufügen.

---
+++ EinfacheSchleifen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinfacheSchleifen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Do-While-Schleifen
+            int zahl = 1;
+
+            do
+            {
+                Console.WriteLine(zahl);
+                zahl += 1;
+            }
+            while (zahl <= 5);
+
+            Console.ReadKey();
+        }
+    }
+}
